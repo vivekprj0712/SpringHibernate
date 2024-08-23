5b7acf927bab9c1dcf470500df28f4be6b574950
vivekprj0712 <prajapativivek949@gmail.com>
2024-08-23 18:55:28 +0530

    validation

--- src/main/java/com/Entity/RestaurantEntity.java
+++ src/main/java/com/Entity/RestaurantEntity.java
@@ -1,11 +1,12 @@
 package com.Entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Restaurants")
@@ -14,8 +15,11 @@ public class RestaurantEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer restaurantId;
+	@NotBlank(message = "Name Can Not Be Null")
 	private String name;
+	@NotBlank(message = "Address Can Not Be Null")
 	private String address;
+	@NotBlank(message = "Category Can Not Be Null")
 	private String category;
 	public Integer getRestaurantId() {
 		return restaurantId;
